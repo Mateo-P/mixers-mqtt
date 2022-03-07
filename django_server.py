7d017e5ca9bcbfcbb2a8fa08c06038dadf3f8516
Mateo Perez <58700246+Mateo-P@users.noreply.github.com>
2022-03-06 20:56:22 -0500

    Feature: send & receive recipe done

--- django_server.py
+++ django_server.py
@@ -1,23 +1,24 @@
 import paho.mqtt.client as mqtt
 
+
 def on_message(client, userdata, message):
-    logOut = open(f'{message.topic}.csv',"a",newline='')
+    logOut = open(f'{message.topic}.csv', "a", newline='')
     print(message.payload)
-    logOut.write(str(message.payload.decode("utf-8"))+ "\n")
+    logOut.write(str(message.payload.decode("utf-8")) + "\n")
     logOut.close()
 
-#Connection to mqtt-broker
+
+# Connection to mqtt-broker
 mqttBroker = "127.0.0.1"
 client = mqtt.Client("django_server")
 client.connect(mqttBroker)
 
 # methods
-def send_recipe_to_mixer(recipe="MMGV;100;200;300;144;123;321;222;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;", mixer_id=1):
-    client.publish(f'mixer/{mixer_id}/newrecipe', recipe,2)
-    print("Just published " + str(recipe) + " to Topic mixer/"+mixer_id)
 
-# Loop to receive messages trought subscriptons
-client.loop_start()
-client.subscribe("mixer/+/recipes_request",2)
-client.on_message = on_message
-client.loop_forever()
+
+def send_recipe_to_mixer(recipe="MMGV2;100;200;300;144;123;321;222;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;", mixer_id=1):
+    client.publish(f'mixer/{mixer_id}/newrecipe', recipe)
+    print(f'Just published {recipe} to Topic mixer/{mixer_id}')
+
+
+send_recipe_to_mixer()
--- mixer.py
+++ mixer.py
@@ -1,29 +1,27 @@
-from email import message
 import paho.mqtt.client as mqtt
-MIXER_ID = '1'
+MIXER_ID = 1
+
 
 def on_message(client, userdata, message):
-    logOut = open("mixer_recipes.csv","a",newline='')
+    logOut = open("mixer_recipes.csv", "a", newline='')
     print(message.payload)
-    logOut.write(str(message.payload.decode("utf-8"))+ "\n")
+    logOut.write(str(message.payload.decode("utf-8")) + "\n")
     logOut.close()
-  
-#Connection to mqtt-broker
+
+
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        client.connected_flag = True  # set flag
+        print("connected OK")
+    else:
+        print("Bad connection Returned code=", rc)
+
+
 mqttBroker = "127.0.0.1"
-client = mqtt.Client(MIXER_ID)
+client = mqtt.Client(str(MIXER_ID))
+client.on_connect = on_connect
 client.connect(mqttBroker)
 
-# methods
-def send_repices_to_server():
-    csv_recipes = open("mixer_recipes.csv","r")
-    recipes =  csv_recipes.readlines()
-    for recipe in recipes:
-        print(recipe)
-    client.publish(f'mixer/{MIXER_ID}/recipes_request', recipes,2)
-    print("Just published " + str(recipe) + " to Topic mixer/"+MIXER_ID)
-
-# Loop to receive messages trought subscriptons
-client.loop_start()
-client.subscribe(f'mixer/{MIXER_ID}/#',2)
+client.subscribe(f'mixer/{MIXER_ID}/#', 2)
 client.on_message = on_message
 client.loop_forever()
