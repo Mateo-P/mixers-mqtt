b2cbe24193b9d1114e74318e1b1c3c85349a65aa
Mateo Perez <58700246+Mateo-P@users.noreply.github.com>
2022-04-17 18:49:42 -0500

    unmount mount

--- mixer.py
+++ mixer.py
@@ -23,6 +23,11 @@ def on_message(client, userdata, message):
     print(message.payload)
     logOut.write(str(message.payload.decode("utf-8")) + "\n")
     logOut.close()
+    unmount = os.system('sudo modprobe -r g_mass_storage')
+    print("`mount` ran with exit code %d" % unmount)
+    mount = os.system(
+        "sudo modprobe g_mass_storage file=/usb-drive.img stall=0 ro=0 removable=1")
+    print("`mount` ran with exit code %d" % mount)
 
 
 # Create an MQTT client and attach our routines to it.
